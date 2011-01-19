951d618e74d884af2da5f6e46290691be2a399e2
Brian Yang <brian@ubuntu.(none)>
2011-01-18 19:33:52 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ColdSpring::Application.config.secret_token = '2f1315bd70c957dc218219e996abe2a67e2b794e49760f232f715b4865079c4c335cf86de6eee046cfd35f292a6e7bf0b04212893212ad0027b6c5d5af1f7a9a'
